7b6b8fe052f1958e8f87b032c2a09cb847017c09
nicdillon <dilloncnicholas@gmail.com>
2024-09-23 16:38:34 -0400

    Reformatted delayed and digitalData code into functions

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,26 +1,43 @@
 
 // add delayed functionality here
-try {
-    import('./digitalData.js')
-    document.querySelector(".cookie-consent-deny").addEventListener("click", function () {
-        document.querySelector("#simple-cookie-consent").style.display = "none"
-    })
-    document.querySelector(".cookie-consent-allow").addEventListener("click", function () {
-        document.querySelector("#simple-cookie-consent").style.display = "none"
-    })
-}
-catch (e) {
-    console.log("No Cookie Consent Box Available!");
+function importDigitalDataScript() {
+    try {
+        import('./digitalData.js')
+    }
+    catch (e) {
+        console.log("error loading digital data script")
+    }
 }
 
-try {
-    if (document.title == 'Home | Healthy Horizons') {
-        document.querySelector("#nav > div.section.nav-sections > div > ul > li:nth-child(1) > a").style.fontWeight = "bold"
+function addCookieConsentEventListener() {
+    try {
+
+        document.querySelector(".cookie-consent-deny").addEventListener("click", function () {
+            document.querySelector("#simple-cookie-consent").style.display = "none"
+        })
+        document.querySelector(".cookie-consent-allow").addEventListener("click", function () {
+            document.querySelector("#simple-cookie-consent").style.display = "none"
+        })
     }
-    else if (window.location.pathname == '/fitness') {
-        document.querySelector("#nav > div.section.nav-sections > div > ul > li:nth-child(2) > a").style.fontWeight = "bold"
+    catch (e) {
+        console.log("No Cookie Consent Box Available!");
     }
 }
-catch {
-    // do nothing
+
+function updateActiveLinkStyling() {
+    try {
+        if (document.title == 'Home | Healthy Horizons') {
+            document.querySelector("#nav > div.section.nav-sections > div > ul > li:nth-child(1) > a").style.fontWeight = "bold"
+        }
+        else if (window.location.pathname == '/fitness') {
+            document.querySelector("#nav > div.section.nav-sections > div > ul > li:nth-child(2) > a").style.fontWeight = "bold"
+        }
+    }
+    catch {
+        // do nothing
+    }
 }
+
+importDigitalDataScript()
+addCookieConsentEventListener()
+updateActiveLinkStyling()
--- scripts/digitalData.js
+++ scripts/digitalData.js
@@ -1,39 +1,55 @@
-try {
-    typeof window.digitalData == "undefined" ? window.digitalData = {} : console.log("DigitalData Available");
-    var clickButtonName = localStorage.getItem("buttonName");
-    var clickButtonHref = localStorage.getItem("buttonHref");
-    if (clickButtonName != null) {
-        window.digitalData.clickEvent = {
-            "buttonName": clickButtonName,
-            "buttonHref": clickButtonHref
+function sendClickEvent() {
+    try {
+        typeof window.digitalData == "undefined" ? window.digitalData = {} : console.log("DigitalData Available");
+        var clickButtonName = localStorage.getItem("buttonName");
+        var clickButtonHref = localStorage.getItem("buttonHref");
+        if (clickButtonName != null) {
+            window.digitalData.clickEvent = {
+                "buttonName": clickButtonName,
+                "buttonHref": clickButtonHref
+            }
+            _satellite.track("clickEvent", digitalData);
+            localStorage.removeItem("buttonName");
+            localStorage.removeItem("buttonHref");
         }
-        _satellite.track("clickEvent", digitalData);
-        localStorage.removeItem("buttonName");
-        localStorage.removeItem("buttonHref");
-    }
-} catch { console.log("Error in Firing Link Click Direct Call"); }
+    } catch { console.log("Error in Firing Link Click Direct Call"); }
+}
 
-try {
-    window.digitalData.page = {
-        "pageInfo": {
-            "pageShortName": document.title.split("|")[document.title.split("|").length - 1],
-            "pageName": window.document.title,
-            "destinationURL": window.location.href,
-            "title": window.document.title,
-            "server": window.location.origin
+function sendPageView() {
+    try {
+        window.digitalData.page = {
+            "pageInfo": {
+                "pageShortName": document.title.split("|")[document.title.split("|").length - 1],
+                "pageName": window.document.title,
+                "destinationURL": window.location.href,
+                "title": window.document.title,
+                "server": window.location.origin
+            }
+        };
+        window.digitalData.page.pageInfo.referringURL = document.referrer;
+        window.digitalData.page.pageInfo.sysEnv = navigator.userAgent;
+        _satellite.track("pageview", digitalData);
+    } catch (e) { console.log("Error in Page View Direct Call") }
+}
+
+function attachLinkCallbacks() {
+    try {
+        var buttons = document.querySelectorAll("a");
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", function () {
+                localStorage.setItem("buttonName", buttons[i].title);
+                localStorage.setItem("buttonHref", buttons[i].href);
+                window.digitalData.clickEvent = {
+                    "buttonName": clickButtonName,
+                    "buttonHref": clickButtonHref
+                }
+                _satellite.track("clickEvent", digitalData);
+            });
         }
-    };
-    window.digitalData.page.pageInfo.referringURL = document.referrer;
-    window.digitalData.page.pageInfo.sysEnv = navigator.userAgent;
-    _satellite.track("pageview", digitalData);
-} catch (e) { console.log("Error in Page View Direct Call") }
-
-try {
-    var buttons = document.querySelectorAll("a");
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function () {
-            localStorage.setItem("buttonName", buttons[i].title);
-            localStorage.setItem("buttonHref", buttons[i].href);
-        });
     }
-} catch (e) { console.log("Error in Setting up Local Storage for link Click"); }
+    catch (e) { console.log("Error in Setting up Local Storage for link Click"); }
+}
+
+sendPageView()
+sendClickEvent()
+attachLinkCallbacks()
